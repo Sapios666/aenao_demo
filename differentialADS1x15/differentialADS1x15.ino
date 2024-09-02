09cb15bf5aac7432239e94d0b11b5a3dd8feab0d
George Margaritis <g.k.margaritis@gmail.com>
2024-09-02 09:09:06 +0300

    Add median filter in arduino code

--- differentialADS1x15/differentialADS1x15.ino
+++ differentialADS1x15/differentialADS1x15.ino
@@ -1,6 +1,10 @@
 #include <Adafruit_ADS1X15.h>
 
-#define MESSAGES
+// #define TEST
+// #define FUNC_PRINT
+
+#define WINDOW 20 // How many samples will be stored in the median filter array
+#define SAMPLING 500 // Sampling period in milliseconds
 
 Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
 // Adafruit_ADS1015 ads;     /* Use this for the 12-bit version */
@@ -11,7 +15,7 @@ float weightVoltageRatio = 4.12; // According to approximations the voltage drop
 void setup(void)
 {
   Serial.begin(115200);
-#ifdef MESSAGES
+#ifdef TEST
   Serial.println("Hello!");
   Serial.println("Getting differential reading from AIN0 (P) and AIN1 (N)");
 #endif
@@ -38,22 +42,64 @@ void loop(void)
 {
   int16_t results;
   float voltage, weight;
+  float filter[WINDOW];
 
   /* Be sure to update this value based on the IC and the gain settings! */
   // float   multiplier = 3.0F;    /* ADS1015 @ +/- 6.144V gain (12-bit results) */
   float multiplier = 0.1875F; /* ADS1115  @ +/- 6.144V gain (16-bit results) */
 
+  /*
   results = ads.readADC_Differential_0_1();
   voltage = results * multiplier;
   weight = (voltage - noLoadOffset) / weightVoltageRatio;
+  */
 
-#ifdef MESSAGES
+#ifdef TEST
   // Serial.print("Differential: "); Serial.print(results); Serial.print("("); Serial.print(results * multiplier); Serial.println("mV)");
   Serial.print(voltage); Serial.print("mV\t");
-  Serial.print(weight); Serial.print("kg\n");
+  // Serial.print(weight); Serial.print("kg\n");
   delay(1000);
 #else
-  Serial.println(weight);
-  delay(100);
+  for(int i=0; i<WINDOW; i++) {
+    results = ads.readADC_Differential_0_1();
+    filter[i] = results * multiplier;
+    // Serial.println(filter[i]);
+    delay(SAMPLING);
+  }
+  Serial.println(median_filter(filter));
+  delay(1000);
 #endif
+}
+
+float median_filter(float * array)
+{
+  float temp;
+
+#ifdef FUNC_PRINT
+  for(int i=0; i<WINDOW; i++) {
+    Serial.print(array[i]);
+    Serial.print(" ");
+  }
+#endif
+
+  for(int i=0; i<WINDOW; i++) {
+    for(int j=i+1; j<WINDOW; j++) {
+      if(array[i]>array[j]) {
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
+
+#ifdef FUNC_PRINT
+  Serial.print("\n");
+  for(int i=0; i<WINDOW; i++) {
+    Serial.print(array[i]);
+    Serial.print(" ");
+  }
+  Serial.print("\n");
+#endif
+
+  return array[(int)WINDOW/2];
 }
